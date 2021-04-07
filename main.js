af13f9343d7fd921803ffb2bcf2b30db91de03a2
javierPerez <javip107@gmail.com>
2021-04-07 11:02:36 +0200

    Nivel-2

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function validacion() {
     }
     else if (!(validarEmail(inputEmail1.value))) {
         inputEmail1.classList.add("is-invalid");
-        document.getElementById("eEmail1").textContent = "Email no aceptado";
+        document.getElementById("eEmail1").textContent = "Este email no existe";
         error++;
     }
 
@@ -41,8 +41,10 @@ function validacion() {
         document.getElementById("ePass1").textContent = "Campo obligatorio";
         error++;
     }
-    else if (false) { //nivel2
-
+    else if (!(validarPassword(inputPassword1.value))) { //nivel2
+        inputPassword1.classList.add("is-invalid");
+        document.getElementById("ePass1").textContent = "Contraseña no valida: Minimo una mayuscula, un numero y 8 caracteres";
+        error++;
     }
     if (inputPassword1.value !== inputPassword2.value) {
         inputPassword2.classList.add("is-invalid");
@@ -72,10 +74,10 @@ function validacion() {
         document.getElementById("eZip").textContent = "Codigo postal no valido";
         error++;
     }
-
+    
     if (!gridCheck.checked) {
         gridCheck.classList.add("is-invalid");
-        document.getElementById("eCheck").textContent = "Acepta las bases";
+        document.getElementById("eCheck").textContent = "Es obligatorio aceptar las condiciones";
         error++;
     }
 
@@ -90,7 +92,14 @@ form.addEventListener('blur', (event) => {
 function validarEmail(email) {
     //Estandard Official: RFC 5322:
     let argu;
-    var re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-    argu = re.test(email);
-    return argu;
+    var re_email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    return re_email.test(email);
+}
+//Nivel 2 Funcion para validar contraseñas 
+// Mínim una majúscula
+// Mínim un número
+// Mínim 8 caràcters
+function validarPassword(pass){
+    let re_pass= /^(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\S{8,}/ 
+    return re_pass.test(pass);
 }
