6d12151711b83c7592ba05931f2d5fe3507ffbc0
javierPerez <javip107@gmail.com>
2021-04-07 11:15:55 +0200

    nivel-3

--- main.js
+++ main.js
@@ -16,68 +16,68 @@ function validacion() {
     form.classList.remove("is-invalid");
     if (inputEmail1.value == "") {
         inputEmail1.classList.add("is-invalid");
-        document.getElementById("eEmail1").textContent = "Campo obligatorio";
+        document.getElementById("eEmail1").textContent = "Campo obligatorio.";
         error++;
     }
     else if (!(validarEmail(inputEmail1.value))) {
         inputEmail1.classList.add("is-invalid");
-        document.getElementById("eEmail1").textContent = "Este email no existe";
+        document.getElementById("eEmail1").textContent = "Este email no es válido.";
         error++;
     }
 
     if (inputEmail2.value == "") {
         inputEmail2.classList.add("is-invalid");
-        document.getElementById("eEmail2").textContent = "Campo obligatorio";
+        document.getElementById("eEmail2").textContent = "Campo obligatorio.";
         error++;
     }
     else if (inputEmail2.value !== inputEmail1.value) {
         inputEmail2.classList.add("is-invalid");
-        document.getElementById("eEmail2").textContent = "Email no coincide";
+        document.getElementById("eEmail2").textContent = "El email no coincide, vuelve a comprobarlo.";
         error++;
     }
 
     if (inputPassword1.value == "") {
         inputPassword1.classList.add("is-invalid");
-        document.getElementById("ePass1").textContent = "Campo obligatorio";
+        document.getElementById("ePass1").textContent = "Campo obligatorio.";
         error++;
     }
     else if (!(validarPassword(inputPassword1.value))) { //nivel2
         inputPassword1.classList.add("is-invalid");
-        document.getElementById("ePass1").textContent = "Contraseña no valida: Minimo una mayuscula, un numero y 8 caracteres";
+        document.getElementById("ePass1").textContent = "Contraseña no valida: Minimo una mayuscula, un numero y 8 caracteres.";
         error++;
     }
     if (inputPassword1.value !== inputPassword2.value) {
         inputPassword2.classList.add("is-invalid");
-        document.getElementById("ePass2").textContent = "Contraseñas no coinciden";
+        document.getElementById("ePass2").textContent = "Contraseñas no coinciden.";
         error++;
     }
 
     if (inputAddress.value == "") {
         inputAddress.classList.add("is-invalid");
-        document.getElementById("eAddress").textContent = "Campo obligatorio";
+        document.getElementById("eAddress").textContent = "Campo obligatorio.";
         error++;
     }
 
     if (inputCity.value == "") {
         inputCity.classList.add("is-invalid");
-        document.getElementById("eCity").textContent = "Campo obligatorio";
+        document.getElementById("eCity").textContent = "Campo obligatorio.";
         error++;
     }
     if (inputZip.value == "") {
         inputZip.classList.add("is-invalid");
-        document.getElementById("eZip").textContent = "Campo obligatorio";
+        document.getElementById("eZip").textContent = "Campo obligatorio.";
         error++;
     }
     //Codigo postal español 5 digitos y esta comprendido entre el 01000 y 52999
     else if (inputZip.value.length != 5 || parseInt(inputZip.value) < 1000 || parseInt(inputZip.value) > 52999) {
         inputZip.classList.add("is-invalid");
-        document.getElementById("eZip").textContent = "Codigo postal no valido";
+        document.getElementById("eZip").textContent = "Codigo postal no valido.";
         error++;
     }
-    
+
     if (!gridCheck.checked) {
         gridCheck.classList.add("is-invalid");
-        document.getElementById("eCheck").textContent = "Es obligatorio aceptar las condiciones";
+        document.getElementById("eCheck").textContent = "Es obligatorio aceptar las condiciones legales.";
         error++;
     }
 
@@ -99,7 +99,8 @@ function validarEmail(email) {
 // Mínim una majúscula
 // Mínim un número
 // Mínim 8 caràcters
-function validarPassword(pass){
-    let re_pass= /^(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\S{8,}/ 
+//Acepta caracteres especiales. Pero no que empiece por ellos. 
+function validarPassword(pass) {
+    let re_pass = /^(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\S{8,}/
     return re_pass.test(pass);
 }
